526cff6096f55dcb9ea89ee0cacabab1474c105f
1010radhika <radhikajadhav1010@gmail.com>
2023-10-10 17:19:07 +0530

    arithmetic
    l
    
    /
    
    }
    ;

---
+++ arithmetic/arith.dart
@@ -0,0 +1,10 @@
+void main()
+{
+ var a=23,b=34,c;
+ print(c=a+b);
+ print(c=a-b);
+ print(c=a*b);
+ print(c=(a/b));
+print(c=(a%b));
+
+}
--- for-loop3/for-loop3.dart
+++ for-loop3/for-loop3.dart
@@ -1,8 +1,8 @@
 void main(){
- for(int i=1;i<=10;i++){
- for(int j=1;j<=10;j++){
-  int n=i*j;
-  print('$n');
- }
-}
+  var a=34,b=19,c;
+   print(c=a+b);
+   print(c=a-b);
+   print(c=a*b);
+   print(c=a/b);
+   print(c=a%b);
 }
